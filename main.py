4d8d8d7c91f74e165703a49d38c66a0a350820b1
Brandon Frazier <bfrazier756@gmail.com>
2025-08-28 19:14:24 -0500

    deleting paul skenes

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ players = [
     {"id": 1, "name": "Shohei Ohtani", "team": "Dodgers"},
     {"id": 2, "name": "Aaron Judge", "team": "Yankees"},
     {"id": 3, "name": "Mookie Betts", "team": "Dodgers"},
-    {"id": 4, "name": "Christian Yelich", "team": "Brewers"},
-    {"id": 5, "name": "Paul Skenes", "team": "Pirates"}
+    {"id": 4, "name": "Christian Yelich", "team": "Brewers"}
 ]
 
 @app.get("/players")
